02c8b5d5dff83c1bf4b53ecb13653b55dbf8684a
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-04 15:32:35 -0300

    Implemented Composite

--- estructural-patterns/src/main/java/br/com/cod3r/composite/swing/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/composite/swing/Client.java
@@ -1,8 +1,27 @@
 package br.com.cod3r.composite.swing;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		JFrame frame =new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+
+		JLabel title = new JLabel("TITLE");
+		frame.add(title, BorderLayout.NORTH);
+
+		JPanel panel = new JPanel(new FlowLayout());
+		JLabel label = new JLabel("Label #1");
+		JButton button= new JButton("Button #1");
+		panel.add(label);
+		panel.add(button);
+
+		frame.add(panel, BorderLayout.CENTER);
+
+		frame.setSize(300,300);
+		frame.setVisible(true);
 	}
 }
